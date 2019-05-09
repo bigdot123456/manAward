acbadc7f43a54ca2e0aef1b587bd7388a2233410
bigdot123456 <bigdot@126.com>
2019-05-09 15:59:07 +0800

    miner selection is ok!

--- SelectMiner.m
+++ SelectMiner.m
@@ -1,12 +1,22 @@
 %% Select Miner with input is MCan and output is default 32 miner with index and name
-function [MIndex,MMiner]=SelectMiner(MCand,SelectNum)
-%% usage: 	[MIndex,MMiner]=SelectMiner(MCand,SelectNum)
+function [MIndex,Miner]=SelectMiner(MCand,SelectNum)
+%% usage: 	[MIndex,Miner]=SelectMiner(MCand,SelectNum)
+%   Miner Data structrue is as following:
+%  fieldnames(MCand)
+%  3¡Á1 cell array
+%
+%    {'Name'   }
+%    {'Staked' }
+%    {'Account'}
+
+%% judge parameter
 	if nargin==1,SelectNum=32;end
 	%disp(SelectNum)
-	Morder=randperm(length(MCand));
+	Morder=randperm(length(MCand.Name));
 	%disp(Morder)
 	MIndex=Morder(1:SelectNum);
-	MMiner=MCand(MIndex);
-
-
-	
+	%% value selected Miner
+    Miner.Name=MCand.Name(MIndex);
+    Miner.Account=MCand.Account(MIndex);
+    Miner.Staked=MCand.Staked(MIndex);
+    
--- SelectMiner_test.m
+++ SelectMiner_test.m
@@ -1,4 +1,25 @@
-a=randi(200,1,100);
-b=1e4+a;
-%c=int2str(b(:));
-[MIndex,MMiner]=SelectMiner(b);
+%% clear environment variable
+clear;
+clc;
+
+%% init miner setting
+NumMiner=500;
+base=10000;
+No=1:NumMiner;
+
+%miner = struct('Name',{},'Staked',{},'Account',{}) ;
+% Error!
+% miner.Name=repmat('Miner',1,NumMiner)+PostName;
+
+%% define miner value
+for i=1:NumMiner
+    miner.Name(i)="Miner"+num2str(i);
+end
+x=miner.Name;
+
+miner.Staked=base+randi(3000,1,NumMiner);
+miner.Account=zeros(1,NumMiner);
+
+%% call Miner
+% fieldnames(miner)
+[MIndex,MMiner]=SelectMiner(miner);
